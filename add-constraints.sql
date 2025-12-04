c799a86c5eda63084eae02b7212c64afe26694a9
VoLaNYeZ <zeyfarid@gmail.com>
2025-12-04 06:25:04 +0300

    lighter

--- add-constraints.sql
+++
@@ -1,6 +0,0 @@
-
-ALTER TABLE aso_entries 
-  ADD CONSTRAINT valid_ranking CHECK (ranking >= 0),
-  ADD CONSTRAINT valid_installs CHECK (installs >= 0),
-  ADD CONSTRAINT valid_cpi CHECK (cpi >= 0 AND cpi <= 1000);
-
